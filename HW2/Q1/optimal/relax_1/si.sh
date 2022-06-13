a8d113ed1ac32c5b52c46a90330e609dc649f5f9
Hanfeng Zhai <frankzhai0@gmail.com>
2022-06-13 18:33:17 -0400

    Add files via upload

---
+++ HW2/Q1/optimal/relax_1/si.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#SBATCH -J Si        # Job Name
+#SBATCH -o Si.o%j    # Name of the error and output files (%j appends the jobID)
+#SBATCH -N 1             # Requests nodes
+#SBATCH -n 10            # Requests number of tasks, in multiples of 64
+#SBATCH -p development   # Queue name: development (max 2 hr walltime) or normal
+#SBATCH -t 00:30:00      # Run time (hh:mm:ss)
+#SBATCH -A TG-MPS150006
+set -x   # Echo commands, use set echo with csh
+
+
+PREFIX="Si_relax1"
+PSEUDO_DIR="./"
+INFILE="$PREFIX.in"
+OUTFILE="$PREFIX.out"
+
+cat > $INFILE << EOF
+&CONTROL
+ calculation = 'vc-relax',
+ restart_mode = 'from_scratch',
+ prefix='$PREFIX',
+ pseudo_dir='$PSEUDO_DIR',
+ tstress=.true.,
+ tprnfor=.true.,
+/
+&SYSTEM
+ ibrav=2,
+ celldm(1)=10.2d0,
+ nat=2,
+ ntyp=1,
+ ecutwfc=20,
+ ecutrho=300,
+ occupations = 'fixed',
+ use_all_frac = .true.,
+/
+&ELECTRONS
+ conv_thr = 1.0D-8,
+ mixing_mode = 'plain',
+ mixing_beta = 0.7d0,
+ diagonalization = 'cg',
+/
+&IONS
+ ion_dynamics='bfgs',
+/
+&CELL
+ cell_dynamics = 'bfgs',
+ press=0.d0,
+/
+ATOMIC_SPECIES
+Si 28.0855d0 Si_LDA.UPF
+
+ATOMIC_POSITIONS crystal
+Si 0.d0 0.d0 0.d0
+Si 0.25d0 0.25d0 0.25d0
+
+K_POINTS automatic
+  8 8 8 0 0 0
+
+EOF
+
+module load intel/19.1.1
+module load impi/19.0.7
+module load mvapich2/2.3.6
+module load qe/7.0
+
+pw.x < $INFILE > $OUTFILE # Run the executable named pw.x 
+
+# rm *.igk *.wfc*
+# rm -r *.save
+
+
