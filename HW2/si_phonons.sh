64731c5989ce4cc62c39195f307380f9b2e96bad
Hanfeng Zhai <frankzhai0@gmail.com>
2022-06-13 18:29:09 -0400

    Add files via upload

---
+++ HW2/si_phonons.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#SBATCH -J Si        # Job Name
+#SBATCH -o Si.o%j    # Name of the error and output files (%j appends the jobID)
+#SBATCH -N 1             # Requests nodes
+#SBATCH -n 32            # Requests number of tasks, in multiples of 64
+#SBATCH -p development   # Queue name: development (max 2 hr walltime) or normal
+#SBATCH -t 00:45:00      # Run time (hh:mm:ss)
+#SBATCH -A TG-MPS150006
+set -x   # Echo commands, use set echo with csh
+
+
+PREFIX="Si"
+PSEUDO_DIR="/work/03674/benedek/stampede2/pseudos/"
+INFILE="$PREFIX.in"
+OUTFILE="$PREFIX.out"
+
+cat > $INFILE << EOF
+&CONTROL
+ calculation = 'scf',
+ restart_mode = 'from_scratch',
+ prefix='$PREFIX',
+ pseudo_dir='$PSEUDO_DIR',
+ tstress=.true.,
+ tprnfor=.true.,
+/
+&SYSTEM
+ ibrav=2,
+ celldm(1)=10.2d0,
+ nat=2,
+ ntyp=1,
+ ecutwfc=50,
+ ecutrho=500,
+ occupations = 'fixed',
+ use_all_frac = .true.,
+/
+&ELECTRONS
+ conv_thr = 1.0D-08,
+ mixing_mode = 'plain',
+ mixing_beta = 0.7d0,
+ diagonalization = 'cg',
+/
+&IONS
+ ion_dynamics='bfgs',
+/
+&CELL
+ cell_dynamics = 'bfgs',
+ press=0.1d0,
+/
+ATOMIC_SPECIES
+Si 28.0855d0 Si_LDA.UPF
+
+ATOMIC_POSITIONS crystal
+Si 0.d0 0.d0 0.d0
+Si 0.25d0 0.25d0 0.25d0
+
+K_POINTS automatic
+  8 8 8 0 0 0
+
+EOF
+
+module load qe
+
+ibrun pw.x < $INFILE > $OUTFILE # Run the executable named pw.x 
+
+INFILE="$PREFIX.phon.in"
+OUTFILE="$PREFIX.phon.out"
+
+cat > $INFILE << EOF
+Phonons of Si
+&inputph
+tr2_ph=1.0d-16,
+amass(1)=28.0855d0,
+ldisp=.true.,
+nq1=4,nq2=4,nq3=4,
+prefix='$PREFIX',
+fildyn='silicon.dyn',
+/
+
+EOF
+
+ibrun ph.x -ndiag 1 < $INFILE > $OUTFILE # Run the executable named ph.x
+
+rm *.igk *.wfc*
+rm -r *.save
+
+
